3de272f305f43cd976e5ad8e3d657c62886ff2fd
Daniel Von Fange <daniel@leancoder.com>
2020-10-13 10:10:26 -0400

    Add static path.

--- eagleproject/eagleproject/settings.py
+++ eagleproject/eagleproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
