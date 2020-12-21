c67e4383e4879fc6620ae22be9ef528263771f99
Mike Shultz <shultzm@gmail.com>
2020-12-21 11:58:19 -0700

    minor cleanup

--- eagleproject/core/blockchain.py
+++ eagleproject/core/blockchain.py
@@ -499,8 +499,7 @@ def maybe_store_stake_withdrawn_record(log, block):
     # Must be a Staked or Withdrawn event
     if (
         log["address"] == OGN_STAKING and
-        log["topics"][0] != SIG_EVENT_STAKED and
-        log["topics"][0] != SIG_EVENT_WITHDRAWN
+        log["topics"][0] not in [SIG_EVENT_STAKED, SIG_EVENT_WITHDRAWN]
     ):
         return None
 
