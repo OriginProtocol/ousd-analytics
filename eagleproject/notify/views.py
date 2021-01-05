87729f080599e204a127885e6592e90f2416a6ca
Mike Shultz <shultzm@gmail.com>
2021-01-05 10:54:20 -0700

    adds event_seen garbage collection

--- eagleproject/notify/views.py
+++ eagleproject/notify/views.py
@@ -1,8 +1,25 @@
+import sys
+from datetime import datetime, timedelta
 from django.http import HttpResponse
 
+from notify.models import EventSeen
 from notify.main import run_all
 
 
 def run_triggers(request):
     run_all()
+    return HttpResponse("ok")
+
+
+def gc(request):
+    """ General garbage collection tasks """
+
+    # event_seen is time sensitive and old entries are irrelevant
+    try:
+        EventSeen.objects.filter(
+            last_seen__gt=datetime.now() - timedelta(hours=2)
+        ).delete()
+    except Exception as err:
+        print(err, file=sys.stderr)
+
     return HttpResponse("ok")
