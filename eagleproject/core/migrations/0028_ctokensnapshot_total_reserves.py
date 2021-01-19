d6692744041bbd1b274c8ec1c7789d14cc5c5a91
Mike Shultz <shultzm@gmail.com>
2021-01-19 12:29:55 -0700

    fix migration deps

--- eagleproject/core/migrations/0028_ctokensnapshot_total_reserves.py
+++ eagleproject/core/migrations/0028_ctokensnapshot_total_reserves.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0026_ctokensnapshot'),
+        ('core', '0027_ognstaked_stake_type'),
     ]
 
     operations = [
