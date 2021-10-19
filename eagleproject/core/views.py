26c757ac5b6587762c77acb57bc201b5722973e4
Domen Grabec <grabec@gmail.com>
2021-10-19 15:35:57 +0200

    bug fix

--- eagleproject/core/views.py
+++ eagleproject/core/views.py
@@ -129,7 +129,7 @@ def reload(request):
     return HttpResponse("ok")
 
 def make_monthly_report(request):
-    if settings.ENABLE_REPORTS != "true":
+    if not settings.ENABLE_REPORTS:
         print("Reports disabled on this instance")
         return HttpResponse("ok")
 
@@ -138,7 +138,7 @@ def make_monthly_report(request):
     return HttpResponse("ok")
 
 def make_weekly_report(request):
-    if settings.ENABLE_REPORTS != "true":
+    if not settings.ENABLE_REPORTS:
         print("Reports disabled on this instance")
         return HttpResponse("ok")
             
@@ -155,7 +155,7 @@ def test_queue(request):
     return HttpResponse("ok")
 
 def make_specific_month_report(request, month):
-    if settings.ENABLE_REPORTS != "true":
+    if not settings.ENABLE_REPORTS:
         print("Reports disabled on this instance")
         return HttpResponse("ok")
 
@@ -163,10 +163,10 @@ def make_specific_month_report(request, month):
     return HttpResponse("ok")
 
 def make_specific_week_report(request, week):
-    if settings.ENABLE_REPORTS != "true":
+    if not settings.ENABLE_REPORTS:
         print("Reports disabled on this instance")
         return HttpResponse("ok")
-        
+
     create_time_interval_report_for_previous_week(week)
     return HttpResponse("ok")
 
