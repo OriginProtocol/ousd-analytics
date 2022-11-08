003f158be7547a7dcb3c630407f35e998ac282de
Shahul Hameed <shahthepro@gmail.com>
2022-11-08 20:54:36 +0530

    cleanup

--- eagleproject/core/blockchain/metastrategies.py
+++ eagleproject/core/blockchain/metastrategies.py
@@ -1,13 +1,8 @@
-# Params: Address, name, LP token 
-# Events:
-# - Pool Balance Change %: 1d, 7d, 30d??
-# - % of pool held??
-
 METASTRATEGIES = [
   {
-    "KEY": "ousd_metastrat",
-    "NAME": "OUSD MetaStrategy",
-    "ADDRESS": "0x89eb88fedc50fc77ae8a18aad1ca0ac27f777a90",
-    "FROM_BLOCK": 15896478
+    "KEY": "ousd_metastrat", # UUID
+    "NAME": "OUSD MetaStrategy", # Shown in the Dashboard
+    "ADDRESS": "0x89eb88fedc50fc77ae8a18aad1ca0ac27f777a90", # Contract address
+    "FROM_BLOCK": 15896478 # Block to listen events from
   },
 ]
