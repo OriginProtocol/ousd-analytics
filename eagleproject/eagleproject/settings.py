f47d7b7164448f1e29f495120d8f1b867f2f9694
Shahul Hameed <shahthepro@gmail.com>
2023-01-07 11:49:59 +0530

    Update static URL

--- eagleproject/eagleproject/settings.py
+++ eagleproject/eagleproject/settings.py
@@ -193,7 +193,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 REPORT_RECEIVER_EMAIL_LIST = os.environ.get("REPORT_RECEIVER_EMAIL_LIST")
