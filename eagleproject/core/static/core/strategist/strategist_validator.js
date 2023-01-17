d03cfd86cb2dc9ba314ffa9412398ecc425d7f50
Micah Alcorn <273937+micahalcorn@users.noreply.github.com>
2023-01-17 10:57:27 -0800

    Repair contract name

--- eagleproject/core/static/core/strategist/strategist_validator.js
+++ eagleproject/core/static/core/strategist/strategist_validator.js
@@ -14,7 +14,7 @@ const strategistValidator = (function () {
     "0x9c459eeb3FA179a40329b81C1635525e9A0Ef094": "Compound Strat",
     "0xEA2Ef2e2E5A749D4A66b41Db9aD85a38Aa264cb3": "Convex 3pool Strat",
     "0x7A192DD9Cc4Ea9bdEdeC9992df74F1DA55e60a19": "Convex LUSD Strat",
-    "0x89Eb88fEdc50FC77ae8a18aAD1cA0ac27f777a90": "Convex OUSC Strat",
+    "0x89Eb88fEdc50FC77ae8a18aAD1cA0ac27f777a90": "Convex OUSD Strat",
     "0x79f2188ef9350a1dc11a062cca0abe90684b0197": "Morpho Aave Strat",
     "0x5A4eEe58744D1430876d5cA93cAB5CcB763C037D": "Morpho Compound Strat",
 
