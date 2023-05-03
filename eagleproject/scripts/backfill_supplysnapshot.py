3a1f09ba07a43e72039cc1f6b27bdec2bf415723
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-05-03 13:07:06 +0400

    Change to a more recent block

--- eagleproject/scripts/backfill_supplysnapshot.py
+++ eagleproject/scripts/backfill_supplysnapshot.py
@@ -1,22 +1,25 @@
 import datetime
 
-from core.blockchain.const import START_OF_OETH
+# from core.blockchain.const import START_OF_OETH
 from core.blockchain.rpc import latest_block
 from core.blockchain.harvest.snapshots import ensure_supply_snapshot
 from core.models import OriginTokens, SupplySnapshot
 
 def run():
     latest = latest_block()
+
+    start_block = 17171685 # The block from which the data got bad
     
     print("Deleting older rows from DB")
     SupplySnapshot.objects.filter(
-        block_number__gte=START_OF_OETH
+        block_number__gte=start_block
     ).delete()
 
-    print("Target block range to backfill: {} to {}".format(START_OF_OETH, latest - 2))
-    for block_number in range(START_OF_OETH, latest - 2):
+    print("Target block range to backfill: {} to {}".format(start_block, latest - 2))
+    for block_number in range(start_block, latest - 2):
         print("Taking snapshot of OUSD Supply on Block {}".format(block_number))
         ensure_supply_snapshot(block_number, project=OriginTokens.OUSD)
+
         print("Taking snapshot of OETH Supply on Block {}".format(block_number))
         ensure_supply_snapshot(block_number, project=OriginTokens.OETH)
 
