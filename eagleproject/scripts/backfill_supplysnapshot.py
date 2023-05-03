c698938847b886f90310d3e029e8e5f161fc16f7
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-05-03 12:49:41 +0400

    Also, drop rows from script

--- eagleproject/scripts/backfill_supplysnapshot.py
+++ eagleproject/scripts/backfill_supplysnapshot.py
@@ -3,10 +3,15 @@ import datetime
 from core.blockchain.const import START_OF_OETH
 from core.blockchain.rpc import latest_block
 from core.blockchain.harvest.snapshots import ensure_supply_snapshot
-from core.models import OriginTokens
+from core.models import OriginTokens, SupplySnapshot
 
 def run():
     latest = latest_block()
+
+    SupplySnapshot.objects.filter(
+        block_number__gte=START_OF_OETH
+    ).delete()
+
     for block_number in range(START_OF_OETH, latest - 2):
         ensure_supply_snapshot(block_number, project=OriginTokens.OUSD)
         ensure_supply_snapshot(block_number, project=OriginTokens.OETH)
