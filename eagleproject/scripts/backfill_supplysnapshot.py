99d944bc8e031c3e5ec37284b15b73adca42d8d1
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-05-03 12:55:06 +0400

    Add logs

--- eagleproject/scripts/backfill_supplysnapshot.py
+++ eagleproject/scripts/backfill_supplysnapshot.py
@@ -7,11 +7,17 @@ from core.models import OriginTokens, SupplySnapshot
 
 def run():
     latest = latest_block()
-
+    
+    print("Deleting older rows from DB")
     SupplySnapshot.objects.filter(
         block_number__gte=START_OF_OETH
     ).delete()
 
+    print("Target block range to backfill: {} to {}".format(START_OF_OETH, latest - 2))
     for block_number in range(START_OF_OETH, latest - 2):
+        print("Taking snapshot of OUSD Supply on Block {}".format(block_number))
         ensure_supply_snapshot(block_number, project=OriginTokens.OUSD)
+        print("Taking snapshot of OETH Supply on Block {}".format(block_number))
         ensure_supply_snapshot(block_number, project=OriginTokens.OETH)
+
+    print("Done.")
