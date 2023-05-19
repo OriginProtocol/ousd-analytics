1de80389daf59011c0f989076dd8295acf653298
Rafael Ugolini <rafael.ugolini@gmail.com>
2023-05-19 23:11:51 +0200

    fix: add decimal decoding for json

---
+++ eagleproject/core/utils.py
@@ -0,0 +1,13 @@
+import json
+from collections.abc import Mapping, Iterable
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def encode(self, obj):
+        if isinstance(obj, Mapping):
+            return '{' + ', '.join(f'{self.encode(k)}: {self.encode(v)}' for (k, v) in obj.items()) + '}'
+        if isinstance(obj, Iterable) and (not isinstance(obj, str)):
+            return '[' + ', '.join(map(self.encode, obj)) + ']'
+        if isinstance(obj, Decimal):
+            return f'{obj.normalize():f}'  # using normalize() gets rid of trailing 0s, using ':f' prevents scientific notation
+        return super().encode(obj)
